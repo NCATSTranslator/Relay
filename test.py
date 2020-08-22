690ff78b404e98c51563e115b8ec26f2a0e00f65
nsouthall <southalln@mail.nih.gov>
2020-08-22 18:22:23 -0400

    init travis server test

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from contextlib import closing
 import requests
 import json
+import os
+
+syscall = "curl -d @tr_sys/tr_ara_unsecret/unsecretAgent.json http://localhost:8000/ars/api/agents"
+fp = os.popen(syscall)
+print(fp.readlines())
+fp.close()
+syscall = "curl -d @tr_sys/tr_ara_unsecret/unsecretActor.json http://localhost:8000/ars/api/actors"
+fp = os.popen(syscall)
+print(fp.readlines())
+fp.close()
+
+
 url ='http://transltr.io:7071/validate_querygraph'
 testJson = json.loads(open('./data/travis.json').read())
 with closing(requests.post(url, json=testJson, stream=False)) as response:
-    
     status_code = response.status_code
 
     assert status_code == 200
