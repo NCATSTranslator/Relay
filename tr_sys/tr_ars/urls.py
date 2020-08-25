9552352e69e30c6c4d8367eb5a1883b535550227
nsouthall <southalln@mail.nih.gov>
2020-08-25 11:54:23 -0400

    use django re_path opt  at path end

--- tr_sys/tr_ars/urls.py
+++ tr_sys/tr_ars/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 from . import api
 
 apipatterns = [
-    path(r'', api.index, name='ars-api'),
-    path(r'submit', api.submit, name='ars-submit'),
-    path(r'messages', api.messages, name='ars-messages'),
-    path(r'agents', api.agents, name='ars-agents'),
-    path(r'actors', api.actors, name='ars-actors'),
-    path(r'channels', api.channels, name='ars-channels'),
-    path(r'agents/<name>', api.get_agent, name='ars-agent'),
-    path(r'messages/<uuid:key>', api.message, name='ars-message'),
+    path('', api.index, name='ars-api'),
+    re_path(r'^submit/?$', api.submit, name='ars-submit'),
+    re_path(r'^messages/?$', api.messages, name='ars-messages'),
+    re_path(r'^agents/?$', api.agents, name='ars-agents'),
+    re_path(r'^actors/?$', api.actors, name='ars-actors'),
+    re_path(r'^channels/?$', api.channels, name='ars-channels'),
+    path('agents/<name>', api.get_agent, name='ars-agent'),
+    path('messages/<uuid:key>', api.message, name='ars-message'),
 ]
 
 urlpatterns = [
