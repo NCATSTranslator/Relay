50413309eb56a2586ac8de7049aa7394e2a149dd
nsouthall <southalln@mail.nih.gov>
2020-08-30 09:05:32 -0400

    cleanup

--- tr_sys/tr_ars/tasks.py
+++ tr_sys/tr_ars/tasks.py
@@ -8,21 +8,6 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 @shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
-
-
-@shared_task
 def send_message(actor_dict, mesg_dict, timeout=60):
     #logger.error(actor_dict)
     logger.info(mesg_dict)
