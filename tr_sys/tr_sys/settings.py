58b9e9a8514ab232e1e613929fab8b55604e481e
MarkDWilliams <markwilliams2755@gmail.com>
2020-08-31 17:22:51 -0400

    initial work.  still not functional

--- tr_sys/tr_sys/settings.py
+++ tr_sys/tr_sys/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'tr_sys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tr_ars','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
