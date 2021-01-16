1fd509e000e20bba093693f6a90011b0cf10d98b
tz <tz@iomics.us>
2021-01-16 01:26:56 -0500

    Update to TRAPI v1.0.0

--- tr_sys/tr_ara_explanatory/explanatory_app.py
+++ tr_sys/tr_ara_explanatory/explanatory_app.py
@@ -4,7 +4,7 @@ from tr_ars.default_ars_app.api import *
 
 class AppConfig(ARSAppConfig):
     name = 'tr_ara_explanatory.explanatory_app' # must be dot path for module
-    actors = [('http://explanatory-agent.azurewebsites.net/agent', 'runquery', 'general')] # tuple of remote, name, channel
+    actors = [('http://explanatory-agent.azurewebsites.net/v1.0/', 'runquery', 'general')] # tuple of remote, name, channel
     app_path = 'ara-explanatory'
     regex_path = '^' + app_path + '/'
 
