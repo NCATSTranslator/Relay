0bcaba9330bda86fc804169253606ab0c16d7663
Phil Owen <phillipsowen@outlook.com>
2021-02-02 12:50:12 -0500

    trying to resolve merge conflict

--- tr_sys/tr_sys/settings.py
+++ tr_sys/tr_sys/settings.py
@@ -53,12 +53,6 @@ INSTALLED_APPS = [
     'tr_kp_cam.cam_app.AppConfig',
     'tr_kp_textmining.textmining_app.AppConfig',
     'tr_ara_aragorn-exp.aragorn-exp_app.AppConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_celery_results',
     'markdownify',
 ]
