94d7befc881737a872dc5069f5f5239cee15300f
MarkDWilliams <markwilliams2755@gmail.com>
2021-08-30 17:12:12 -0400

    Update startup.sh
    Unneeded changes from my local.  Reverting to the way they are in the master branch

--- config/startup.sh
+++ config/startup.sh
@@ -2,7 +2,7 @@
 
 
 # makemigrations command is changed to run locally and commit migrations file into github repo
-python tr_sys/manage.py makemigrations tr_ars
+#python tr_sys/manage.py makemigrations tr_ars
 
-#python tr_sys/manage.py migrate
+python tr_sys/manage.py migrate
 #python tr_sys/manage.py createsuperuser
