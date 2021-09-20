71cdc2b09727e3c55a5aed64bd2fe70007f57b00
Williams <markwilliams2755@gmail.com>
2021-09-20 15:17:12 -0400

    reverting URL changes to synchronous

--- tr_sys/tr_ara_arax/arax_app.py
+++ tr_sys/tr_ara_arax/arax_app.py
@@ -4,7 +4,7 @@ from tr_ars.default_ars_app.api import *
 
 class AppConfig(ARSAppConfig):
     name = 'tr_ara_arax.arax_app' # must be dot path for module
-    actors = [('https://arax.ncats.io/api/arax/v1.2/asyncquery', 'runquery', 'general')] # tuple of remote, name, channel
+    actors = [('https://arax.ncats.io/api/arax/v1.2/query', 'runquery', 'general')] # tuple of remote, name, channel
     app_path = 'ara-arax'
     regex_path = '^' + app_path + '/'
 
