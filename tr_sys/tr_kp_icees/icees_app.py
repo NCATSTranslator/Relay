4ebff3548544014ad7b5f3bab0e6b7cc6bb0aa20
Williams <markwilliams2755@gmail.com>
2022-03-04 15:44:48 -0500

    updating icees main URL

--- tr_sys/tr_kp_icees/icees_app.py
+++ tr_sys/tr_kp_icees/icees_app.py
@@ -4,7 +4,7 @@ from tr_ars.default_ars_app.api import *
 
 class AppConfig(ARSAppConfig):
     name = 'tr_kp_icees.icees_app' # must be dot path for module
-    actors = [('https://icees.renci.org:16339/query?reasoner=true&verbose=false', 'runquery', 'general')] # tuple of remote, name, channel
+    actors = [('https://icees-asthma.renci.org/query?reasoner=true&verbose=false', 'runquery', 'general')] # tuple of remote, name, channel
     app_path = 'kp-icees'
     regex_path = '^' + app_path + '/'
 
