f8fb16eeb6d0dc1c215d051dce871c79a77c1f2b
Williams <markwilliams2755@gmail.com>
2022-04-01 15:50:11 -0400

    minor fix, fumble fingers

--- tr_sys/tr_sys/urls.py
+++ tr_sys/tr_sys/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 import json
 from django.conf import settings
-from django.conf.urls import include, re_path
+from django.urls import include, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
