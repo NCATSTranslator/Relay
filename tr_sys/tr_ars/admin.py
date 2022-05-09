c33bb6cae292522f896d0e638a2ad87f5f7bc5e4
abdollahis2 <abdollahis2@ncats-2170731-m.myfiosgateway.com>
2022-05-09 15:05:39 -0400

    multi channel actor feature addition

--- tr_sys/tr_ars/admin.py
+++ tr_sys/tr_ars/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Agent, Channel, Actor, Message, ActorIntermediateModel
+from .models import Agent, Channel, Actor, Message
 
 admin.site.register(Agent)
 admin.site.register(Channel)
 admin.site.register(Actor)
 admin.site.register(Message)
-admin.site.register(ActorIntermediateModel)
+
--- tr_sys/tr_ars/models.py
+++ tr_sys/tr_ars/models.py
@@ -35,7 +35,7 @@ class Channel(ARSModel):
         return self.name
 
 class Actor(ARSModel):
-    channels = models.ManyToManyField(Channel, through='ActorIntermediateModel')
+    channel = models.ManyToManyField(Channel) #, through='ActorIntermediateModel'
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     path = models.CharField('relative path of actor', max_length=64)
     inforesid = models.CharField('inforesid', blank=True, max_length=500)
@@ -58,14 +58,14 @@ class Actor(ARSModel):
         jsonobj['fields']['url'] = self.url()
         return jsonobj
 
-class ActorIntermediateModel(ARSModel):
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
-    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+#class ActorIntermediateModel(ARSModel):
+ #   channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+  #  actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+  #  created_at = models.DateTimeField(auto_now_add=True)
+  #  updated_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('channel', 'actor')
+    #class Meta:
+       # unique_together = ('channel', 'actor')
 
 class Message(ARSModel):
     STATUS = (
