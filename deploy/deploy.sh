df2f7e727c4c08dd44cfaf529e8c55c0a39b7d29
aojesanmi <97059224+aojesanmi@users.noreply.github.com>
2022-05-10 09:49:20 -0400

    feat: Refactor NCATS ARS K8s deployment to helm chart installation (#236)
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * Update deploy.sh
    
    * feat added helm chart
    
    * feat added helm chart
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * feat added helm chart
    
    * Update deployment.yaml
    
    * Update values.yaml
    
    * Update values.yaml
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deployment.yaml
    
    * Update deploy.sh
    
    * Update Jenkinsfile
    
    * Update deploy.sh
    
    * Update deployment.yaml
    
    * Update deploy.sh
    
    * Update deployment.yaml
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deployment.yaml
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * feat added helm chart
    
    * fix: removed old deployment files
    
    * fix: modified values file
    
    * fix: modified values file
    
    * fix: modified files arrangement
    
    * fix: modified files arrangement
    
    * fix: modified files arrangement
    
    * fix: modified files arrangement
    
    * fix: modified files arrangement
    
    * fix: modified files arrangement
    
    * fix: modified file arrangement
    
    * fix: modified file arrangement
    
    * fix: modified file arrangement
    
    * fix: modified file name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,38 +1,11 @@
-#!/bin/bash
-
-export $(egrep -v '^#' .env)
+namespace="ars"
 
 sed -i.bak \
-    -e "s/ARS_ALLOWED_HOSTS_VALUE/${ARS_ALLOWED_HOSTS}/g" \
-    -e "s/ARS_DATABASE_VALUE/${ARS_DATABASE}/g" \
-    -e "s/ARS_DB_USER_VALUE/${ARS_DB_USER}/g" \
-    -e "s/ARS_DB_PASSWORD_VALUE/${ARS_DB_PASSWORD}/g" \
-    -e "s/ARS_DB_HOST_VALUE/${ARS_DB_HOST}/g" \
-    -e "s/ARS_DJANGO_SECRET_KEY_VALUE/${ARS_DJANGO_SECRET_KEY}/g" \
-    -e "s/ARS_SETTINGS_DEFAULT_HOST_VALUE/${ARS_SETTINGS_DEFAULT_HOST}/g" \
-    settings.py
-rm settings.py.bak
-
-CONFIG_HASH="$(shasum settings.py | cut -d ' ' -f 1 | tr -d '\n')"
-
-sed -i.bak \
-    -e "s/CONFIG_HASH_VALUE/${CONFIG_HASH}/g" \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
-    -e "s/TR_ENV_VALUE/${TR_ENV}/g" \
-    deployment.yaml
-rm deployment.yaml.bak
-
-sed -i.bak \
-    -e "s/ARS_HOSTNAME_VALUE/${ARS_HOSTNAME}/g" \
-    -e "s/ARS_ALB_TAG_VALUE/${ARS_ALB_TAG}/g" \
-    -e "s/ARS_ALB_SG_VALUE/${ARS_ALB_SG}/g" \
-    -e "s/ENVIRONMENT_TAG_VALUE/${ENVIRONMENT_TAG}/g" \
-    ingress.yaml
-rm ingress.yaml.bak
+    values.yaml
+rm values.yaml.bak
 
 kubectl apply -f namespace.yaml
-kubectl delete configmap ars-config -n ars
-kubectl create configmap ars-config -n ars --from-file=settings.py --dry-run -o yaml | kubectl apply -f -
-kubectl apply -f deployment.yaml
-kubectl apply -f services.yaml
-kubectl apply -f ingress.yaml
+
+# deploy helm chart
+helm upgrade --install -n ${namespace} -f values-ncats.yaml ars . 
