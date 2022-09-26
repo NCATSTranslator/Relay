e94b5297861174517a8a724a96d16859583cc4ce
abdollahis2 <ShervinAbd92>
2022-09-26 09:31:41 -0400

    build after mysql installation on jenkins

--- tr_sys/factories.py
+++ tr_sys/factories.py
@@ -8,36 +8,36 @@ logger.setLevel(logging.INFO)
 fake = Faker()
 
 
-# class AgentFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = models.Agent
-#         django_get_or_create = ("name",)
-#     name = 'agent_default'
-#     uri = 'ara-example/api/'
+class AgentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Agent
+        django_get_or_create = ("name",)
+    name = 'agent_default'
+    uri = 'ara-example/api/'
 
 class ChannelFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Channel
     name = 'test'
     description = fake.text()
-#
-# class ActorFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = models.Actor
-#
-#     agent = factory.SubFactory(AgentFactory)
-#     channel = 'test'
-#     path = 'runquery'
-#     inforesid = fake.word()
-#     active = 'True'
-#
-# class MessageFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = models.Message
-#
-#     id = fake.uuid4()
-#     name = 'default_message'
-#     status = 'W'
-#     actor = factory.SubFactory(ActorFactory)
+
+class ActorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Actor
+
+    agent = factory.SubFactory(AgentFactory)
+    channel = 'test'
+    path = 'runquery'
+    inforesid = fake.word()
+    active = 'True'
+
+class MessageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Message
+
+    id = fake.uuid4()
+    name = 'default_message'
+    status = 'W'
+    actor = factory.SubFactory(ActorFactory)
 
 
