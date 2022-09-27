1b21148b9edc3dc136ecf3a38c1b9229eb90fa42
abdollahis2 <ShervinAbd92>
2022-09-27 15:29:21 -0400

    added integration testing after aws deployment

--- tr_sys/tr_ars/tests/integration/test_integration.py
+++ tr_sys/tr_ars/tests/integration/test_integration.py
@@ -1,11 +1,21 @@
 import requests
-from django.contrib.auth.models import AnonymousUser, User
-from django.test import RequestFactory, TestCase
+import os
 
-# DEFAULT_HOST = 'https://ars-prod.transltr.io'
-# def test_response():
-#     response = requests.get(DEFAULT_HOST+"/ars/api/agents")
-#     print(response)
-#     response_body = response.json()
-#     print(response_body)
-#     assert (response_body[0])["fields"]["name"] == "ara-aragorn"
+hostname=os.getenv('TARGET_HOST')
+def test_agent_endopoint():
+    response = requests.get("https://"+hostname+"/ars/api/agents")
+    response_body = response.json()
+    agent_names=[]
+    for agent in response_body:
+        agent_names.append(agent['fields']['name'])
+    print(agent_names)
+    assert "ara-aragorn" in agent_names
+
+
+def test_actor_endpoint():
+    response = requests.get("https://"+hostname+"/ars/api/actors")
+    response_body = response.json()
+    acotr_agent_names=[]
+    for actor in response_body:
+        acotr_agent_names.append(actor['fields']['agent'])
+    assert "ara-aragorn" in acotr_agent_names
