f8fdc5287b7a3312a9b58185eeeede042c09caf0
Ojesanmi <ojesanmiaa@nih.gov>
2023-01-04 12:42:32 -0500

    fix: modify deploy.sh file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,15 @@ sed -i.bak \
     values.yaml
 rm values.yaml.bak 
 
+sed -i.bak \
+    -e "s/USERNAME_VALUE/$USERNAME/g;s/PASSWORD_VALUE/$PASSWORD/g" \
+    -e "s/ENGINE_VALUE/$ENGINE/g;s/DEFAULT_HOST_VALUE/$DEFAULT_HOST/g" \
+    -e "s/DBNAME_VALUE/$DBNAME/g;s/HOST_VALUE/$HOST/g" \
+    -e "s/PORT_VALUE/$PORT/g;s/SECRET_KEY_VALUE/$SECRET_KEY/g" \
+    -e "s/CSRF_TRUSTED_ORIGINS_VALUE/$CSRF_TRUSTED_ORIGINS/g" \
+    configs/settings.py
+rm configs/settings.py.bak 
+
 kubectl apply -f namespace.yaml
 
 # deploy helm chart
