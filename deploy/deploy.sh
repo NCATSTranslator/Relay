865c4e22b84fefcf22dbafbb787589700d82ae7b
Ojesanmi <ojesanmiaa@nih.gov>
2023-01-04 12:02:40 -0500

    fix: modify deploy.sh file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,8 +11,8 @@ sed -i.bak \
     -e "s/DBNAME_VALUE/$DBNAME/g;s/HOST_VALUE/$HOST/g" \
     -e "s/PORT_VALUE/$PORT/g;s/SECRET_KEY_VALUE/$SECRET_KEY/g" \
     -e "s/CSRF_TRUSTED_ORIGINS_VALUE/$CSRF_TRUSTED_ORIGINS/g" \
-    deploy/configs/settings.py
-rm deploy/configs/settings.py.bak    
+    configs/settings.py
+rm configs/settings.py.bak    
 
 kubectl apply -f namespace.yaml
 
