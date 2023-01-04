e9b2e4f63df5359aa7fb2942f272876d534c4764
Ojesanmi <ojesanmiaa@nih.gov>
2023-01-04 13:34:37 -0500

    fix: modify deploy.sh file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,7 @@ sed -i.bak \
 rm values.yaml.bak 
 
 sed -i.bak \
-    -e "s/SECRET_KEY_VALUE/$SECRET_KEY/g;s/CSRF_TRUSTED_ORIGINS_VALUE/$CSRF_TRUSTED_ORIGINS/g" \
+    -e "s/SECRET_KEY_VALUE/$SECRET_KEY/g" \
     -e "s/ENGINE_VALUE/$ENGINE/g;s/DBNAME_VALUE/$DBNAME/g" \
     -e "s/USERNAME_VALUE/$USERNAME/g;s/PASSWORD_VALUE/$PASSWORD/g" \
     -e "s/HOST_VALUE/$HOST/g;s/PORT_VALUE/$PORT/g" \
