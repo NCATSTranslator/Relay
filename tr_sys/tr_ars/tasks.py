461ec32b94f5107de969697cb878cd5c036b6fe9
abdollahis2 <ShervinAbd92>
2023-01-26 09:57:21 -0500

    changed status check timeout to 60 min

--- tr_sys/tr_ars/tasks.py
+++ tr_sys/tr_ars/tasks.py
@@ -139,7 +139,7 @@ def send_message(actor_dict, mesg_dict, timeout=300):
 @shared_task(name="catch_timeout")
 def catch_timeout_async():
     now =timezone.now()
-    time_threshold = now - timezone.timedelta(minutes=10)
+    time_threshold = now - timezone.timedelta(minutes=60)
     messages = Message.objects.filter(timestamp__lt=time_threshold, status__in='R')
     for mesg in messages:
         agent = str(mesg.actor.agent.name)
