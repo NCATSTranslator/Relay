e7c53db67f0677a76194a9fb4fa83a46db09613f
MarkDWilliams <markwilliams2755@gmail.com>
2023-01-31 16:33:40 -0500

    Update startup.sh

--- config/startup.sh
+++ config/startup.sh
@@ -3,7 +3,5 @@
 
 # makemigrations command is changed to run locally and commit migrations file into github repo
 #python tr_sys/manage.py makemigrations tr_ars
-python tr_sys/manage.py migrate --fake tr_ars 0006_message_result_stat
-python tr_sys/manage.py migrate --fake tr_ars 0007_auto_20230131_1846
-python tr_sys/manage.py migrate
+python tr_sys/manage.py migrate --fake
 #python tr_sys/manage.py createsuperuser
