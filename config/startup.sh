b0bd913fcee81387a02055fa954cfb992703475f
abdollahis2 <ShervinAbd92>
2023-02-03 11:57:24 -0500

    removed --fake from startup script

--- config/startup.sh
+++ config/startup.sh
@@ -3,5 +3,5 @@
 
 # makemigrations command is changed to run locally and commit migrations file into github repo
 #python tr_sys/manage.py makemigrations tr_ars
-python tr_sys/manage.py migrate --fake
+python tr_sys/manage.py migrate 
 #python tr_sys/manage.py createsuperuser
