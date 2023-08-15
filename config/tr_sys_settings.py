8b2d19ea4ed96b274617ddf4a035c406572fdb13
abdollahis2 <ShervinAbd92>
2023-08-15 15:58:53 -0400

    added health check functionality

--- config/tr_sys_settings.py
+++ config/tr_sys_settings.py
@@ -61,6 +61,11 @@ INSTALLED_APPS = [
     'tr_kp_icees_kg.icees_kg_app.AppConfig',
     'django_celery_results',
     'markdownify',
+    'health_check',
+    'health_check.contrib.celery',
+    'health_check.contrib.celery_ping',
+    'health_check.contrib.rabbitmq',
+    'health_check.contrib.redis',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +111,7 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
+REDIS_URL = "redis://redis:6379"
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -207,7 +212,7 @@ STATICFILES_DIRS = (
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_BROKER_URL = 'pyamqp://rabbitmq:5672'
-
+BROKER_URL = 'pyamqp://rabbitmq:5672'
 # Other important shared settings
 
 USE_CELERY = True
