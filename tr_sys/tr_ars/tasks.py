7edcd5eae661081753dcd6739a084cff291a4d62
Williams <markwilliams2755@gmail.com>
2023-08-18 11:03:06 -0400

    Fixing inforesid in edge decoration

--- tr_sys/tr_ars/tasks.py
+++ tr_sys/tr_ars/tasks.py
@@ -118,7 +118,7 @@ def send_message(actor_dict, mesg_dict, timeout=300):
                         message_to_merge=rdata
                         agent_name = str(mesg.actor.agent.name)
 
-                        utils.pre_merge_process(message_to_merge,mesg_dict['pk'], agent_name)
+                        utils.pre_merge_process(message_to_merge,mesg_dict['pk'], agent_name, inforesid)
                         mesg.code = status_code
                         mesg.status = status
                         mesg.data = rdata
