df3467900aa5c7ad96aa34d71544848f3c815a6a
Williams <markwilliams2755@gmail.com>
2024-02-26 13:11:15 -0500

    Exempting merged_versions from timeout

--- tr_sys/tr_ars/tasks.py
+++ tr_sys/tr_ars/tasks.py
@@ -191,8 +191,8 @@ def catch_timeout_async():
         id = mesg[1]
         actor = Agent.objects.get(pk=mpk)
         logging.info(f'actor: {actor} id: {mesg[1]} timestamp: {mesg[2]} updated_at {mesg[3]}')
-
-        if actor.name == 'ars-default-agent':
+        #exempting parents and merged_versions from timing out
+        if actor.name == 'ars-default-agent' or actor.name =='ars-ars-agent':
             continue
         else:
             logging.info(f'for actor: {actor.name}, and pk {str(id)} the status is still "Running" after 5 min, setting code to 598')
