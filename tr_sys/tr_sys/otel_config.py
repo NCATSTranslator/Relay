4244f3162396b40160cc7477d2ea88fd8771f0ce
abdollahis2 <ShervinAbd92>
2024-07-03 09:13:18 -0400

    added some logging for otel config

--- tr_sys/tr_sys/otel_config.py
+++ tr_sys/tr_sys/otel_config.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from opentelemetry import trace
 from opentelemetry.sdk.resources import SERVICE_NAME as telemetery_service_name_key, Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -12,34 +13,32 @@ def configure_opentelemetry():
 
     #jaeger_host = os.environ.get('JAEGER_HOST', 'jaeger-otel-agent')
     #jaeger_port = int(os.environ.get('JAEGER_PORT', '6831'))
-    jaeger_host= 'jaeger-otel-agent.sri'
-    jaeger_port= 6831
-    resource = Resource.create({telemetery_service_name_key: 'ARS'})
+    logging.info('About to instrument ARS app for OTEL')
+    try:
+        jaeger_host= 'jaeger-otel-agent.sri'
+        jaeger_port= 6831
+        resource = Resource.create({telemetery_service_name_key: 'ARS'})
 
-    trace.set_tracer_provider(TracerProvider(resource=resource))
+        trace.set_tracer_provider(TracerProvider(resource=resource))
 
-    tracer_provider = trace.get_tracer_provider()
+        tracer_provider = trace.get_tracer_provider()
 
-    # Configure Jaeger Exporter
-    jaeger_exporter = JaegerExporter(
-        agent_host_name=jaeger_host,
-        agent_port=jaeger_port,
-    )
+        # Configure Jaeger Exporter
+        jaeger_exporter = JaegerExporter(
+            agent_host_name=jaeger_host,
+            agent_port=jaeger_port,
+        )
 
-    span_processor = BatchSpanProcessor(jaeger_exporter)
-    tracer_provider.add_span_processor(span_processor)
+        span_processor = BatchSpanProcessor(jaeger_exporter)
+        tracer_provider.add_span_processor(span_processor)
 
-    #if we want the default POST changes to sth customize, we can add hooks to the Request Instrumentation
-    # def request_hook(span, request, result):
-    #     if span and request.method == "POST":
-    #         span.update_name(f"Custom POST to {request.url}")
-    # # Instrument requests with the custom hook
-    # RequestsInstrumentor().instrument(request_hook=request_hook)
+        # Optional: Console exporter for debugging
+        console_exporter = ConsoleSpanExporter()
+        tracer_provider.add_span_processor(BatchSpanProcessor(console_exporter))
 
-    # Optional: Console exporter for debugging
-    console_exporter = ConsoleSpanExporter()
-    tracer_provider.add_span_processor(BatchSpanProcessor(console_exporter))
-
-    DjangoInstrumentor().instrument()
-    CeleryInstrumentor().instrument()
-    RequestsInstrumentor().instrument()
+        DjangoInstrumentor().instrument()
+        CeleryInstrumentor().instrument()
+        RequestsInstrumentor().instrument()
+        logging.info('Finished instrumenting ARS app for OTEL')
+    except Exception as e:
+        logging.error('OTEL instrumentation failed because: %s'%str(e))
