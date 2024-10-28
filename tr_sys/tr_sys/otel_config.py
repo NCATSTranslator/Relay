c9ca4ea0adfbcc0b0c60b3c010cf15bc7882866d
abdollahis2 <ShervinAbd92>
2024-10-28 13:00:04 -0400

    swtiched from jaeger to OTLP exporter

--- tr_sys/tr_sys/otel_config.py
+++ tr_sys/tr_sys/otel_config.py
@@ -3,9 +3,9 @@ import logging
 from opentelemetry import trace
 from opentelemetry.sdk.resources import SERVICE_NAME as telemetery_service_name_key, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from celery.signals import worker_process_init
@@ -13,12 +13,10 @@ from celery.signals import worker_process_init
 
 def configure_opentelemetry():
 
-    #jaeger_host = os.environ.get('JAEGER_HOST', 'jaeger-otel-agent')
-    #jaeger_port = int(os.environ.get('JAEGER_PORT', '6831'))
     logging.info('About to instrument ARS app for OTEL')
     try:
-        jaeger_host= 'jaeger-otel-agent.sri'
-        jaeger_port= 6831
+        otlp_host = os.environ.get('OTLP_HOST', 'jaeger-otel-collector.sri')
+        otlp_port = int(os.environ.get('OTLP_PORT', '4317'))
         service_name= 'ARS'
         resource = Resource.create({telemetery_service_name_key: service_name})
 
@@ -27,12 +25,11 @@ def configure_opentelemetry():
         tracer_provider = trace.get_tracer_provider()
 
         # Configure Jaeger Exporter
-        jaeger_exporter = JaegerExporter(
-            agent_host_name=jaeger_host,
-            agent_port=jaeger_port,
+        OTLP_exporter = OTLPSpanExporter(
+            endpoint=f"http://{otlp_host}:{otlp_port}"
         )
 
-        span_processor = SimpleSpanProcessor(jaeger_exporter)
+        span_processor = BatchSpanProcessor(OTLP_exporter)
         tracer_provider.add_span_processor(span_processor)
 
         # Optional: Console exporter for debugging
