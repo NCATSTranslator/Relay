0266befba77e06c45bf963ca061f87c284dc89d3
abdollahis2 <ShervinAbd92>
2025-12-05 15:51:09 -0500

    changes to how we save parent to done with all ARAs return 0 results

--- tr_sys/tr_ars/signals.py
+++ tr_sys/tr_ars/signals.py
@@ -4,10 +4,10 @@ from django.dispatch import receiver
 import sys, logging
 from .models import Actor, Agent, Message, Channel
 from .pubsub import send_messages
-from .utils import get_safe
+from .utils import get_safe, createMessage
 from django.utils import timezone
 logger = logging.getLogger(__name__)
-from .api import query_event_unsubscribe
+from .api import query_event_unsubscribe,get_ars_actor
 
 @receiver(post_save, sender=Actor)
 def actor_post_save(sender, instance, **kwargs):
@@ -23,6 +23,9 @@ def actor_post_save(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Message)
 def message_post_save(sender, instance, **kwargs):
+    # --- Prevent recursion / internal saves ---
+    if getattr(instance, "_skip_post_save", False):
+        return
     message = instance
     if message.status == 'R':
         message.code = 202
@@ -72,11 +75,35 @@ def message_post_save(sender, instance, **kwargs):
                 logger.info('Children count is: %s.' % (str(len(children))))
                 logger.info('Merge count is:  %s' % (str(merge_count)))
                 logger.info('Original count is: %s.' % (str(orig_count)))
-                pmessage.status = 'D'
-                pmessage.code = 200
-                pmessage.updated_at = timezone.now()
-                pmessage.save(update_fields=['status','code','updated_at'])
-                query_event_unsubscribe(None, pmessage.pk)
+                #create an empty merged message
+                if merge_count==0 and orig_count==0:
+                    empty_merged_mesg= createMessage(get_ars_actor(),str(pmessage.pk))
+                    empty_data = pmessage.decompress_dict()
+                    empty_data['message']['results']=[]
+                    empty_data['message']['auxiliary_graphs']={}
+                    empty_data['message']['knowledge_graph']={}
+                    empty_data['message']['knowledge_graph']= {}
+                    empty_data['message']['knowledge_graph']['edges']={}
+
+                    empty_merged_mesg._skip_post_save = True
+                    empty_merged_mesg.save_compressed_dict(empty_data)
+
+                    empty_merged_mesg.code=200
+                    empty_merged_mesg.status='D'
+                    empty_merged_mesg._skip_post_save = True
+                    empty_merged_mesg.save()
+                    pmessage.merged_version=empty_merged_mesg
+                    pmessage.status = 'D'
+                    pmessage.code = 200
+                    pmessage.updated_at = timezone.now()
+                    pmessage._skip_post_save = True
+                    pmessage.save(update_fields=['status','code','updated_at', 'merged_version'])
+                else:
+                    pmessage.status = 'D'
+                    pmessage.code = 200
+                    pmessage.updated_at = timezone.now()
+                    pmessage.save(update_fields=['status','code','updated_at'])
+                    query_event_unsubscribe(None, pmessage.pk)
             elif pmessage.status == 'E':
                 query_event_unsubscribe(None, pmessage.pk)
             else:
